198abce0717d3d5b0753334974739ac20fe8a7c9
“b2” <mizukake213@gmail.com>
2020-05-16 11:19:44 +0900

    events commit

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 
 import { increment, decrement } from "../actions";
 
-class App extends Component {
+class EventsIndex extends Component {
   render() {
     const props = this.props;
     return (
@@ -23,4 +23,4 @@ const mapDispatchToProps = (dispatch) => ({
 });
 //省略型＝＞ const mapDispatchToProps = ({ increment , decrement })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex);
