8d9a47935e43a580b4874d28f964816d02aa1491
“b2” <mizukake213@gmail.com>
2020-05-16 19:43:49 +0900

    竜児ありがとう

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,4 @@
-import { axios } from "axios";
+import axios from "axios";
 export const READ_EVENTS = "READ_EVENTS";
 const ROOT_URL = "https://udemy-utils.herokuapp.com/api/v1";
 const QUERYSTRING = "?token=token123";
